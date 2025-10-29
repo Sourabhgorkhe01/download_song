f8cf8232ad4d822e2de29a470511e95616b0ee93
Sourabhgorkhe01 <sourabhgorkhecommercial@gmail.com>
2025-10-29 19:01:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 import os
 
-# Bot Token - Get from environment variable
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
-# Download directory
 DOWNLOAD_DIR = os.path.join(os.path.dirname(__file__), 'downloads')
-
-# Create download directory if it doesn't exist
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
